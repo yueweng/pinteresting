f633b7da048ee155ef2c990a43e758fbea96193c
yueweng <yueweng.mak@gmail.com>
2014-09-14 16:30:56 -0700

    creating home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ Pinteresting::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
