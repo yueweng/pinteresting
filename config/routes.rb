924085e900502df241a9a868fc9dff5645c77519
yueweng <yueweng.mak@gmail.com>
2014-09-14 22:31:52 -0700

    clean up pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  resources :pins
+
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
